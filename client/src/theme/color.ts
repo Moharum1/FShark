49b213b50beb5e0db35d690eb590a759461bfc0c
moharum1 <moharum4@gmail.com>
2026-05-30 14:54:52 +0100

    Basic Tailwind setup and added a screen display plain text

---
+++ client/src/theme/color.ts
@@ -0,0 +1,6 @@
+export const colors = {
+    primary: "#1A1F2C",
+    secondary: "#F5F5F0",
+    tertiary: "#2D5A27",
+    neutral: "#707070"
+}
---
+++ client/src/theme/typography.ts
@@ -0,0 +1,23 @@
+export const typography = {
+    fontFamily: {
+        Headline: "Playfair Display",
+        Body: "Inter",
+        Label: "Inter",
+    },
+
+    fontSize: {
+        xs: '0.75rem',   // 12px
+        sm: '0.875rem',  // 14px
+        md: '1rem',      // 16px
+        lg: '1.125rem',  // 18px
+        xl: '1.25rem',   // 20px
+        xxl: '2rem',     // 32px
+    },
+
+    fontWeight: {
+        regular: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+    },
+}
